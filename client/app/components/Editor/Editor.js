e83c6b743b8eb932825d97a0ea7ea953c2b09000
lopun <ujin43255252@gmail.com>
2018-09-13 16:45:02 +0900

    Editor function done

--- client/app/components/Editor/Editor.js
+++ client/app/components/Editor/Editor.js
@@ -5,13 +5,65 @@ import styles from "./styles.scss";
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      script: "",
+      option: "c",
+      result: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit() {
+    console.log(this.state.script);
+    console.log(this.state.option);
+    fetch("http://localhost:8080/api/compile/script", {
+      method: "POST",
+      body: JSON.stringify({
+        script: `${this.state.script}`,
+        option: this.state.option
+      }),
+      headers: { "Content-Type": "application/json" }
+    })
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          result: json.result.stdout
+        });
+      });
   }
 
   render() {
     return (
       <>
-        <div />
+        <div>
+          <textarea
+            onChange={this.handleChange}
+            name="script"
+            value={this.state.script}
+          />
+          <select
+            name="option"
+            onChange={this.handleChange}
+            value={this.state.option}
+            name="option"
+          >
+            <option value="c">C</option>
+            <option value="cpp">C++</option>
+            <option value="java">Java</option>
+            <option value="python">Python</option>
+            <option value="js">Javascript</option>
+          </select>
+          <button onClick={this.handleSubmit}>코드 돌리기</button>
+          <div>{this.state.result}</div>
+        </div>
       </>
     );
   }
